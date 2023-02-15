117ff3544a965cb17a5688b998a8213cb4f06e22
user-azizul <124841821+user-azizul@users.noreply.github.com>
2023-02-15 03:18:52 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-
+const result = document.getElementById("result");
+const decrement = document.getElementById("decrement");
+const increment = document.getElementById("increment");
+const app = document.querySelector(".app");
+let count = 0;
+result.innerText = count;
+increment.addEventListener("click", function (e) {
+  count++;
+  console.log(count);
+  result.innerText = count;
+});
+decrement.addEventListener("click", function (e) {
+  count--;
+  console.log(count);
+  result.innerText = count;
+});
