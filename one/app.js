5d553ce7208bcfa91d5649c61db97c1a283e0b59
Azizu Haque <azizul.contact.me@gmail.com>
2023-02-18 01:36:47 +0300

    The app is done

--- one/app.js
+++ one/app.js
@@ -4,11 +4,13 @@ const increment = document.getElementById("increment");
 const app = document.querySelector(".app");
 let count = 0;
 result.innerText = count;
+// Increment functionality
 increment.addEventListener("click", function (e) {
   count++;
   console.log(count);
   result.innerText = count;
 });
+//decrement functionality
 decrement.addEventListener("click", function (e) {
   count--;
   console.log(count);
