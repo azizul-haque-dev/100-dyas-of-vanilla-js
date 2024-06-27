d3c6b9fe3f96df67edd57e3c415193e468ad5122
user-azizul <azizul.contact.me@gmail.com>
2024-06-27 23:43:48 +0300

    edit some portion

--- 016.stopwatch/app.js
+++ 016.stopwatch/app.js
@@ -5,12 +5,12 @@ let second = document.getElementById('second');
 let minute = document.getElementById('minute');
 let
 function start() {
-  stop();
-  timerInterval = setInterval(function() {
+  
+timerInterval = setInterval(function() {
     timer += 1/60;
-    msVal = Math.floor((timer - Math.floor(timer))*100);
-    secondVal = Math.floor(timer) - Math.floor(timer/60) * 60;
-    minuteVal = Math.floor(timer/60);
+  let  msVal = Math.floor((timer - Math.floor(timer))*100);
+   let secondVal = Math.floor(timer) - Math.floor(timer/60) * 60;
+   let minuteVal = Math.floor(timer/60);
     ms.innerHTML = msVal < 10 ? "0" + msVal.toString() : msVal;
     second.innerHTML = secondVal < 10 ? "0" + secondVal.toString() : secondVal;
     minute.innerHTML = minuteVal < 10 ? "0" + minuteVal.toString() : minuteVal;
